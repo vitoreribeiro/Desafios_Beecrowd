dded7aec68a79c3b064d2ea4e76a85466aa9c2df
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-09-27 21:42:00 -0300

    URI_1035 add

---
+++ URI_1035/src/Main.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int D = sc.nextInt();
+
+
+        if (B > C && D > A && (C + D) > (A + B) && C > 0 && D > 0 && A % 2 == 0) {
+            System.out.println("Valores aceitos");
+        }
+        else {
+            System.out.println("Valores nao aceitos");
+        }
+
+        sc.close();
+
+    }
+
+}
