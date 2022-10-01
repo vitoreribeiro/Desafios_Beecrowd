7973d12eb8e0657d20e024770216ff64c61f9278
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-10-01 18:11:29 -0300

    URI_1038 add

---
+++ URI_1038/src/Main.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+        double total = 0;
+
+        switch (codigo) {
+            case 1:
+                total = quantidade * 4.00;
+                break;
+            case 2:
+                total = quantidade * 4.50;
+                break;
+            case 3:
+                total = quantidade * 5.00;
+                break;
+            case 4:
+                total = quantidade * 2.00;
+                break;
+            case 5:
+                total = quantidade * 1.50;
+                break;
+            default:
+                System.out.println("Numero invalido");
+        }
+
+        System.out.printf("Total: R$ %.2f%n", total);
+
+
+        sc.close();
+    }
+
+}
