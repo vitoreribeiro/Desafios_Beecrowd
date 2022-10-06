d6ee5e6bdbf59b41656a3981e6fae7efc98e7a5e
vitoreribeiro <vitor.e.ribeirosilva@gmail.com>
2022-10-06 07:55:24 -0300

    URI_1044 add

---
+++ URI_1044/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT:
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (B % A == 0) {
+            System.out.println("Sao Multiplos");
+        }
+        else if (A % B == 0) {
+                System.out.println("Sao Multiplos");
+            }
+        else {
+            System.out.println("Nao sao Multiplos");
+        }
+
+        sc.close();
+    }
+
+}
